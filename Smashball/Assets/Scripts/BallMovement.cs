e3f56f19ffe575f45aa45f404f9ce58025d031b9
Corbin Phelps <cphelps8907@gmail.com>
2015-09-12 11:00:59 -0400

    Initial commit

---
+++ Smashball/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		mRigidbody = GetComponent<Rigidbody>();
+		mRigidbody.AddForce( Vector3.forward * 2 );
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		mRigidbody.AddForce( mRigidbody.velocity.normalized * 2 ); 
+		mRigidbody.velocity = Vector3.ClampMagnitude( mRigidbody.velocity, 20 );
+	}
+
+	private Rigidbody mRigidbody = null;
+
+	[SerializeField]
+	private float mMaxVelocity = 25;
+}
---
+++ Smashball/Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate( Vector2.right * -movementSpeed * Time.deltaTime );
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate( Vector2.right * movementSpeed * Time.deltaTime );
+		}
+
+		if (Input.GetKey(KeyCode.Q))
+		{
+			transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * -rotationSpeed );  
+		}
+		else if (Input.GetKey(KeyCode.E))
+		{
+			transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * rotationSpeed );  
+		}
+	}
+
+	[SerializeField]
+	private float rotationSpeed = 50;
+
+	[SerializeField]
+	private float movementSpeed = 2;
+}
