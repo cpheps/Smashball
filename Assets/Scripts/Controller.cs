1cdef1e56a95154f02b690ec5e3f92b1dc3b258c
Corbin Phelps <cphelps8907@gmail.com>
2015-09-20 20:48:19 -0400

    Added xbox 360 controls for Windows (Rotation might not work on OSX). Also, gave paddles the ability to move forward and back just for test purposes.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -7,46 +7,95 @@ public class Controller : MonoBehaviour {
     // Update is called once per frame
     void Update () 
     {
+        float horizontalInput = 0;
+        float rotateInput = 0;
+        float verticalInput = 0;
+
         if ( isPlayerOne )
         {
-            if ( Input.GetKey( KeyCode.A ) )
+            if ( useController )
             {
-                transform.Translate( Vector2.right * -movementSpeed * Time.deltaTime );
+                horizontalInput = Input.GetAxis( "Horizontal One" );
+                verticalInput = Input.GetAxis( "Vertical One" );
+                rotateInput = Input.GetAxis( "Rotate Right One" ) + -Input.GetAxis( "Rotate Left One" );
             }
-            else if ( Input.GetKey( KeyCode.D ) )
+            else
             {
-                transform.Translate( Vector2.right * movementSpeed * Time.deltaTime );
-            }
+                //Keyboard horizontal movement
+                if ( Input.GetKey( KeyCode.A ) )
+                {
+                    horizontalInput = -1; 
+                }
+                else if ( Input.GetKey( KeyCode.D ) )
+                {
+                    horizontalInput = 1;
+                }
 
-            if ( Input.GetKey( KeyCode.Q ) )
-            {
-                transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * -rotationSpeed );
-            }
-            else if ( Input.GetKey( KeyCode.E ) )
-            {
-                transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * rotationSpeed );
+                if ( Input.GetKey( KeyCode.S ) )
+                {
+                    verticalInput = -1;
+                }
+                else if ( Input.GetKey( KeyCode.W ) )
+                {
+                    verticalInput = 1;
+                }
+
+                //Keyboard rotation
+                if ( Input.GetKey( KeyCode.Q ) )
+                {
+                    rotateInput = -1;
+                }
+                else if ( Input.GetKey( KeyCode.E ) )
+                {
+                    rotateInput = 1;
+                }
             }
         }
         else
         {
-            if ( Input.GetKey( KeyCode.J ) )
+            if ( useController )
             {
-                transform.Translate( Vector2.right * -movementSpeed * Time.deltaTime );
+                horizontalInput = Input.GetAxis( "Horizontal Two" );
+                verticalInput = Input.GetAxis( "Vertical Two" );
+                rotateInput = Input.GetAxis( "Rotate Right Two" ) + -Input.GetAxis( "Rotate Left Two" );
             }
-            else if ( Input.GetKey( KeyCode.L ) )
+            else
             {
-                transform.Translate( Vector2.right * movementSpeed * Time.deltaTime );
-            }
+                //Keyboard horizontal movement
+                if ( Input.GetKey( KeyCode.J ) )
+                {
+                    horizontalInput = -1;
+                }
+                else if ( Input.GetKey( KeyCode.L ) )
+                {
+                    horizontalInput = 1;
+                }
 
-            if ( Input.GetKey( KeyCode.U ) )
-            {
-                transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * -rotationSpeed );
-            }
-            else if ( Input.GetKey( KeyCode.O ) )
-            {
-                transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * rotationSpeed );
+                if ( Input.GetKey( KeyCode.K ) )
+                {
+                    verticalInput = -1;
+                }
+                else if ( Input.GetKey( KeyCode.I ) )
+                {
+                    verticalInput = 1;
+                }
+
+                //Keyboard rotation
+                if ( Input.GetKey( KeyCode.U ) )
+                {
+                    rotateInput = -1;
+                }
+                else if ( Input.GetKey( KeyCode.O ) )
+                {
+                    rotateInput = 1;
+                }
             }
         }
+
+        transform.Translate( Vector3.right * movementSpeed * Time.deltaTime * horizontalInput );
+        transform.Translate( Vector3.forward * movementSpeed * Time.deltaTime * verticalInput );
+
+        transform.RotateAround( transform.position, Vector3.up, Time.deltaTime * rotationSpeed * rotateInput );
     }
 
     [SerializeField]
@@ -57,5 +106,8 @@ public class Controller : MonoBehaviour {
 
     [SerializeField]
     private bool isPlayerOne = true;
+
+    [SerializeField]
+    private bool useController = false;
     #endregion
 }
